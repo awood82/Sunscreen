23633e30d9fd9ff10fb3f2c5f9c751f9e0a35c2a
Andrew Wood <ajwood1@gmail.com>
2022-09-22 15:08:23 -0700

    Change MinuteTimer back to 1 minute instead of using the testing value to check the progress bar

--- app/src/main/java/com/androidandrew/sunscreen/tracker/sunburn/MinuteTimer.kt
+++ app/src/main/java/com/androidandrew/sunscreen/tracker/sunburn/MinuteTimer.kt
@@ -5,15 +5,15 @@ import java.util.*
 class MinuteTimer(private val timerTask: TimerTask) : Timer() {
 
     companion object {
-        private val ONE_SECOND = 1000L // milliseconds in one second
-        private val ONE_MINUTE = 60 * ONE_SECOND // millis in one minute
+        private const val ONE_SECOND = 1000L // milliseconds in one second
+        private const val ONE_MINUTE = 60 * ONE_SECOND // millis in one minute
     }
 
     private var _timer = Timer()
 
     fun start() {
         _timer = Timer()
-        _timer.scheduleAtFixedRate(timerTask, ONE_SECOND*5, ONE_SECOND*5)
+        _timer.scheduleAtFixedRate(timerTask, ONE_MINUTE, ONE_MINUTE)
     }
 
     fun stop() {
