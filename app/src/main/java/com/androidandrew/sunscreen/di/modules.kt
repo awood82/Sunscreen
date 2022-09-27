d13232dd3dd80310bc8abd1f8f43aee0e56225fe
Andrew Wood <ajwood1@gmail.com>
2022-09-27 00:10:59 -0700

    Remove nighttime testing helper code

--- app/src/main/java/com/androidandrew/sunscreen/di/modules.kt
+++ app/src/main/java/com/androidandrew/sunscreen/di/modules.kt
@@ -8,9 +8,6 @@ import java.time.Clock
 import java.time.Instant
 import java.time.ZoneId
 
-private val noon = Instant.parse("2022-09-25T15:00:00.00Z")
-private val clockDefaultNoon = Clock.fixed(noon, ZoneId.of("UTC"))
-
 val appModule = module {
     viewModel { MainViewModel(EpaApi.service, Clock.systemDefaultZone()) }
 }
