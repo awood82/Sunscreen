3e8ae98b1bf55e998f394ed0f36da1ac40b0f006
Andrew Wood <ajwood1@gmail.com>
2022-10-14 20:52:07 -0700

    Add Jetpack Navigation components

--- app/src/androidTest/java/com/androidandrew/sunscreen/util/BaseUiTest.kt
+++ app/src/androidTest/java/com/androidandrew/sunscreen/util/BaseUiTest.kt
@@ -8,12 +8,13 @@ import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.fragment.app.testing.withFragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread
 import androidx.test.platform.app.InstrumentationRegistry
-import com.androidandrew.sharedtest.database.FakeDatabase
 import com.androidandrew.sharedtest.network.FakeEpaService
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
@@ -21,12 +22,13 @@ import org.junit.Before
 import org.junit.runner.RunWith
 import com.androidandrew.sunscreen.R
 import org.junit.After
+import org.junit.Assert.assertNotNull
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 abstract class BaseUiTest {
 
-//    lateinit var navController: NavController
+    lateinit var navController: NavController
 
     @Before
     open fun setup() {
@@ -41,37 +43,36 @@ abstract class BaseUiTest {
     }
 
     private fun setupNavController() {
-        /*navController = TestNavHostController(
+        navController = TestNavHostController(
             ApplicationProvider.getApplicationContext()
         )
         runOnUiThread {
             navController.setGraph(R.navigation.navigation)
-        }*/
+        }
     }
 
     protected inline fun <reified T: Fragment> launchFragmentUnderTest(): FragmentScenario<T> {
         val scenario = launchFragmentInContainer<T>(themeResId = R.style.Theme_Sunscreen)
-  /*      scenario.withFragment {
+        scenario.withFragment {
             try {
                 Navigation.setViewNavController(this.requireView(), navController)
             } catch (e: IllegalStateException) {
 
             }
         }
-        */
         runBlocking { delay(100) } // account for initialization in viewModelScope
         return scenario
     }
-/*
+
     protected inline fun <reified T: DialogFragment> launchDialogFragmentUnderTest(): FragmentScenario<T> {
-        val scenario = with (launchFragment<T>(themeResId = R.style.AppTheme)) {
+        val scenario = with (launchFragment<T>(themeResId = R.style.Theme_Sunscreen)) {
             onFragment { fragment ->
                 assertNotNull(fragment.dialog)
             }
         }
         return scenario
     }
-*/
+
     protected fun openOptionsMenu() {
         try {
             Espresso.openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().targetContext)
