f1d92d0e213d59ff22d0e3e4e5f6baa315d3ee96
Andrew <ajwood1@gmail.com>
2023-01-15 12:13:13 -0800

    See logs during instrumentation test runs.

--- app/src/androidTest/java/com/androidandrew/sunscreen/TestApplication.kt
+++ app/src/androidTest/java/com/androidandrew/sunscreen/TestApplication.kt
@@ -5,6 +5,7 @@ import com.androidandrew.sunscreen.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class TestApplication : Application() {
 
@@ -17,5 +18,22 @@ class TestApplication : Application() {
             // Order matters! Overriding modules go last.
             modules(allModules)
         }
+
+        Timber.plant(object : Timber.DebugTree() {
+            // Override [log] to modify the tag and add a "global tag" prefix to it.
+            // You can rename the String "global_tag_" as you see fit.
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                super.log(priority, "$tag", message, t)
+            }
+
+            // Override [createStackElementTag] to include a add a "method name" to the tag.
+            override fun createStackElementTag(element: StackTraceElement): String {
+                return String.format(
+                    "%s:%s",
+                    element.methodName,
+                    super.createStackElementTag(element)
+                )
+            }
+        })
     }
 }
