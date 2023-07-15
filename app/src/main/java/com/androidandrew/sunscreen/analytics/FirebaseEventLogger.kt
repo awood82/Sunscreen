9f20e39c376b353bce1e081d5df08aff2ca24d98
Andrew <ajwood1@gmail.com>
2023-07-15 13:28:21 -0700

    Move Firebase logging keys into enums Log locations that have error results Remove quasi-duplicate screen view tracking

--- app/src/main/java/com/androidandrew/sunscreen/analytics/FirebaseEventLogger.kt
+++ app/src/main/java/com/androidandrew/sunscreen/analytics/FirebaseEventLogger.kt
@@ -22,44 +22,43 @@ class FirebaseEventLogger(
     override fun viewScreen(name: String) {
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, Bundle().apply {
             putString(FirebaseAnalytics.Param.SCREEN_NAME, name)
-            putString(FirebaseAnalytics.Param.SCREEN_CLASS, name + "Screen")
         })
     }
 
     override fun selectLocation(location: String) {
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, Bundle().apply {
-            putString(FirebaseAnalytics.Param.CONTENT_TYPE, "location")
+            putString(FirebaseAnalytics.Param.CONTENT_TYPE, ContentType.LOCATION.name)
             putString(FirebaseAnalytics.Param.ITEM_ID, location)
         })
     }
 
     override fun selectSkinType(skinType: Int) {
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, Bundle().apply {
-            putString(FirebaseAnalytics.Param.CONTENT_TYPE, "skin_type")
+            putString(FirebaseAnalytics.Param.CONTENT_TYPE, ContentType.SKIN_TYPE.name)
             putString(FirebaseAnalytics.Param.ITEM_ID, skinType.toString())
         })
     }
 
     override fun selectClothing(clothing: UserClothing) {
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, Bundle().apply {
-            putString(FirebaseAnalytics.Param.CONTENT_TYPE, "top")
+            putString(FirebaseAnalytics.Param.CONTENT_TYPE, ContentType.TOP.name)
             putString(FirebaseAnalytics.Param.ITEM_ID, clothing.top.name)
         })
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, Bundle().apply {
-            putString(FirebaseAnalytics.Param.CONTENT_TYPE, "bottom")
+            putString(FirebaseAnalytics.Param.CONTENT_TYPE, ContentType.BOTTOM.name)
             putString(FirebaseAnalytics.Param.ITEM_ID, clothing.bottom.name)
         })
     }
 
     override fun selectSpf(spf: Int) {
-        firebaseAnalytics.logEvent("select_spf", Bundle().apply {
-            putInt("spf", spf)
+        firebaseAnalytics.logEvent(Event.SELECT_SPF.name, Bundle().apply {
+            putInt(Param.SPF.name, spf)
         })
     }
 
     override fun selectReflectiveSurface(isReflective: Boolean) {
-        firebaseAnalytics.logEvent("select_reflective_surface", Bundle().apply {
-            putBoolean("is_reflective", isReflective)
+        firebaseAnalytics.logEvent(Event.SELECT_REFLECTIVE_SURFACE.name, Bundle().apply {
+            putBoolean(Param.IS_REFLECTIVE.name, isReflective)
         })
     }
 
@@ -70,19 +69,24 @@ class FirebaseEventLogger(
     }
 
     override fun searchSuccess(location: String) {
-        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_SEARCH_RESULTS, Bundle().apply {
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event. VIEW_SEARCH_RESULTS, Bundle().apply {
             putString(FirebaseAnalytics.Param.SEARCH_TERM, location)
         })
     }
 
     override fun searchError(location: String, error: String?) {
         // TODO: Add Crashlytics?
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event. VIEW_SEARCH_RESULTS, Bundle().apply {
+            putString(FirebaseAnalytics.Param.SEARCH_TERM, location)
+            putString(Param.ERROR_LOCATION.name, location)
+            putString(Param.ERROR.name, error)
+        })
     }
 
     override fun startTracking() {
         // TODO: What to log? Time? Burn and IU?
         trackingStartTimeMs = System.currentTimeMillis()
-        firebaseAnalytics.logEvent("tracking_start", null)
+        firebaseAnalytics.logEvent(Param.TRACKING_START.name, null)
     }
 
     override fun finishTracking() {
@@ -91,8 +95,29 @@ class FirebaseEventLogger(
             TimeUnit.MILLISECONDS.toMinutes((System.currentTimeMillis() - it))
         } ?: -1
         trackingStartTimeMs = null
-        firebaseAnalytics.logEvent("tracking_finish", Bundle().apply {
+        firebaseAnalytics.logEvent(Param.TRACKING_FINISH.name, Bundle().apply {
             putLong("time", elapsedTime)
         })
     }
+}
+
+private enum class Event(name: String) {
+    SELECT_REFLECTIVE_SURFACE("select_reflective_surface"),
+    SELECT_SPF("select_spf"),
+}
+
+private enum class Param(name: String) {
+    ERROR("error"),
+    ERROR_LOCATION("error_location"),
+    IS_REFLECTIVE("is_reflective"),
+    SPF("spf"),
+    TRACKING_START("tracking_start"),
+    TRACKING_FINISH("tracking_finish")
+}
+
+private enum class ContentType(name: String) {
+    LOCATION("location"),
+    SKIN_TYPE("skin_type"),
+    TOP("top"),
+    BOTTOM("bottom")
 }
